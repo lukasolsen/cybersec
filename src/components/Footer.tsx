089c9c2a00a20b7b70626fffbf9088e55197dbd0
lukasolsen <lukmarwil@gmail.com>
2024-05-13 12:47:29 +0200

    (fix): Styling and more sosials

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -29,6 +29,40 @@ export const Footer: React.FC = () => {
         },
       ],
     },
+    {
+      title: "Contact Us",
+      links: [
+        {
+          title: "Address",
+          value: "Satkhira, Kaliganji, BD",
+        },
+        {
+          title: "Email",
+          value: "aburaihan.it009@gmail.com",
+        },
+        {
+          title: "Phone",
+          value: "+88 01407152619",
+        },
+      ],
+    },
+    {
+      title: "Follow Us",
+      links: [
+        {
+          title: "Facebook",
+          link: "https://facebook.com",
+        },
+        {
+          title: "Twitter",
+          link: "https://twitter.com",
+        },
+        {
+          title: "Linkedin",
+          link: "https://linkedin.com",
+        },
+      ],
+    },
   ];
 
   return (
@@ -46,29 +80,29 @@ export const Footer: React.FC = () => {
           </p>
         </div>
         {Fields.map((field) => (
-          <div className="flex flex-col gap-2">
-            <h1 className="text-2xl font-bold">{field.title}</h1>
-            {field.links.map((link) => {
-              if (link.value)
-                return (
-                  <span className="text-gray-500 dark:text-gray-400">
-                    {link.title}
-
-                    <span className="text-teal-500 dark:text-teal-400">
-                      {link.value}
+          <div className="flex flex-col gap-2 justify-center items-center">
+            <div className="flex flex-col gap-1 justify-center">
+              <h1 className="text-2xl font-bold justify-start">
+                {field.title}
+              </h1>
+              {field.links.map((link) => {
+                if (link.value)
+                  return (
+                    <span className="text-gray-500 dark:text-gray-400">
+                      {link.title}: <span className="ml-4">{link.value}</span>
                     </span>
-                  </span>
-                );
+                  );
 
-              return (
-                <Link
-                  to={link.link}
-                  className="text-gray-500 dark:text-gray-400 dark:hover:text-teal-400 transition-colors duration-300"
-                >
-                  {link.title}
-                </Link>
-              );
-            })}
+                return (
+                  <Link
+                    to={link.link}
+                    className="text-gray-500 dark:text-gray-400 dark:hover:text-teal-400 transition-colors duration-300"
+                  >
+                    {link.title}
+                  </Link>
+                );
+              })}
+            </div>
           </div>
         ))}
       </div>
