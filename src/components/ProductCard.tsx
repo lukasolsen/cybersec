a2f8061d8547133fe124db2abd5b740d98524e4b
Lukas Olsen <lukmarwil@gmail.com>
2024-05-13 19:41:07 +0200

    Update package.json with new dependencies and versions

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,7 +1,6 @@
 type ProductCardProps = {
   title: string;
   icon: React.ReactNode;
-
   description: string;
 };
 
@@ -11,14 +10,16 @@ export const ProductCard: React.FC<ProductCardProps> = ({
   title,
 }) => {
   return (
-    <div className="border rounded-lg h-full">
+    <div className="relative border border-r- rounded-lg overflow-hidden">
+      {/* Border gradient */}
+      <div className="absolute inset-0 from-[#fca5a5] to-[#9cdbff] mix-blend-multiply"></div>
+
       <span className="z-10 size-16 flex justify-center items-center w-full">
         {icon}
       </span>
 
-      <div className="p-4 flex flex-col justify-between h-full">
+      <div className="p-4 flex flex-col justify-between">
         <h1 className="text-2xl font-bold">{title}</h1>
-
         <p className="text-gray-400">{description}</p>
       </div>
     </div>
