8c5d2d8d7adc999766d1f18e7a83bc7f7e77b5bf
lukasolsen <lukmarwil@gmail.com>
2024-05-15 10:30:25 +0200

    Update package.json with new dependencies and versions

--- src/main.tsx
+++ src/main.tsx
@@ -14,6 +14,14 @@ const router = createRouter({
   routeTree,
   defaultPreload: "intent",
   defaultPreloadStaleTime: 0,
+
+  notFoundMode: "root",
+  defaultErrorComponent: () => (
+    <div className="p-4 border border-teal-500 rounded-lg">
+      <h1 className="text-2xl font-bold">404 Not Found</h1>
+      <p className="text-lg">The page you are looking for does not exist.</p>
+    </div>
+  ),
 });
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -4,7 +4,87 @@ import { Navbar } from "../components/Navbar";
 import { Footer } from "../components/Footer";
 import { Copyright } from "../components/Copyright";
 
+import "vanilla-cookieconsent/dist/cookieconsent.css";
+import * as CookieConsent from "vanilla-cookieconsent";
+import { useEffect } from "react";
+
 export const RootPage: React.FC = () => {
+  useEffect(() => {
+    CookieConsent.run({
+      guiOptions: {
+        consentModal: {
+          layout: "box",
+          position: "bottom left",
+          equalWeightButtons: true,
+          flipButtons: false,
+        },
+        preferencesModal: {
+          layout: "box",
+          position: "right",
+          equalWeightButtons: true,
+          flipButtons: false,
+        },
+      },
+      categories: {
+        necessary: {
+          readOnly: true,
+        },
+        analytics: {},
+      },
+      language: {
+        default: "no",
+        autoDetect: "browser",
+        translations: {
+          no: {
+            consentModal: {
+              title: "Les dette før du går videre!",
+              description:
+                "Vi bruker <a href='/cookies'>informasjonskapsler</a> for å gi deg en bedre opplevelse og forbedre tjenestene våre.<br><br>Hvis du velger «Godta alle», samtykker du i bruken av informasjonskapsler.<br><br>Hvis du velger avis alle, bruker vi ikke informasjonskapsler bortsett fra de som er nødvendige for at nettstedet skal fungere.",
+              acceptAllBtn: "Godta alle",
+              acceptNecessaryBtn: "Avis alle",
+              showPreferencesBtn: "Tilpass innstillinger",
+              footer:
+                '<a href="/privacy">Personvernregler</a>\n<a href="/terms">Vilkår for bruk</a>',
+            },
+            preferencesModal: {
+              title: "Dine personverninnstillinger",
+              acceptAllBtn: "Godta alle",
+              acceptNecessaryBtn: "Avis alle",
+              savePreferencesBtn: "Lagre innstillinger",
+              closeIconLabel: "Lukk",
+              serviceCounterLabel: "Tjeneste|Tjenester",
+              sections: [
+                {
+                  title: "Bruk av informasjonskapsler",
+                  description:
+                    "Informasjonskapsler er små datafiler som lagres på enheten din og brukes til å forbedre nettstedsopplevelsen din. Vi bruker informasjonskapsler for å tilpasse innhold og annonser, gi sosiale mediefunksjoner og analysere trafikken vår. Ved å godta alle informasjonskapsler, samtykker du i vår bruk av dem. Du kan også velge å avvise visse typer informasjonskapsler.",
+                },
+                {
+                  title:
+                    'Strengt nødvendige informasjonskapsler <span class="pm__badge">Alltid aktivert</span>',
+                  description:
+                    "Disse informasjonskapslene er nødvendige for at nettstedet skal fungere og kan ikke slås av i våre systemer. De brukes vanligvis bare som svar på handlinger du har gjort som innebærer en forespørsel om tjenester, for eksempel å angi personverninnstillinger, logge inn eller fylle ut skjemaer.",
+                  linkedCategory: "necessary",
+                },
+                {
+                  title: "Analyseinformasjonskapsler",
+                  description:
+                    "Disse informasjonskapslene lar oss telle besøk og trafikkilder, slik at vi kan måle og forbedre ytelsen til nettstedet vårt. De hjelper oss å vite hvilke sider som er mest og minst populære, og se hvordan besøkende beveger seg rundt på nettstedet.",
+                  linkedCategory: "analytics",
+                },
+                {
+                  title: "Mer informasjon",
+                  description:
+                    'For spørsmål angående vår bruk av informasjonskapsler og dine valg, vennligst <a class="cc__link" href="#yourdomain.com">kontakt oss</a>.',
+                },
+              ],
+            },
+          },
+        },
+      },
+    });
+  });
+
   return (
     <div className="min-h-screen min-w-full dark:bg-primary dark:text-white">
       <Navbar />
