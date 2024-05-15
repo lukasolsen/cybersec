012f7b7d8b987c05106459e0dc27864fee611c71
lukasolsen <lukmarwil@gmail.com>
2024-05-15 12:34:39 +0200

    Update Admin component with messages state and API call in admin.tsx

--- src/routes/admin.tsx
+++ src/routes/admin.tsx
@@ -3,8 +3,15 @@ import { api } from "../utils/axios";
 import LoginForm from "../components/Admin/Login";
 import { useEffect, useState } from "react";
 
+type Message = {
+  id: string;
+  name: string;
+  text: string;
+};
+
 const Admin = () => {
   const [loggedIn, setLoggedIn] = useState(false);
+  const [messages, setMessages] = useState<Message[]>([]);
 
   useEffect(() => {
     const getData = async () => {
@@ -16,14 +23,38 @@ const Admin = () => {
       }
     };
 
-    getData();
+    const getMessages = async () => {
+      try {
+        const response = await api.get("/messages/all");
+        setMessages(response.data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    getData().then(() => getMessages());
   }, []);
 
   if (!loggedIn) {
     return <LoginForm />;
   }
 
-  return <div className="p-20">logged in</div>;
+  return (
+    <div className="p-20">
+      <h1 className="text-3xl font-bold">Admin Panel</h1>
+
+      <div>
+        <ul className="flex flex-col gap-4 py-2">
+          {messages.map((message) => (
+            <li key={message.id} className="border-t border-b border-gray-500">
+              <h2 className="font-semibold">{message.name}</h2>
+              <p className="text-gray-300">{message.text}</p>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export const Route = createFileRoute("/admin")({
